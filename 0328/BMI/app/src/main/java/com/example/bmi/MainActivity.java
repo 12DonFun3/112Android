44cb6c0d567eee229af5329d0677cd967bdaba3a
12DonFun3 <a111221038@mail.shu.edu.tw>
2024-03-28 17:04:37 +0800

    0328

--- 0328/BMI/app/src/main/java/com/example/bmi/MainActivity.java
+++ 0328/BMI/app/src/main/java/com/example/bmi/MainActivity.java
@@ -10,7 +10,7 @@ import android.widget.EditText;
 import android.widget.TextView;
 
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+public class MainActivity extends AppCompatActivity{
     private TextView txvShow;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,35 +19,57 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         txvShow = (TextView) findViewById(R.id.txvShow) ;
         txvShow.setTextSize(36);
+        EditText edtH = (EditText) findViewById(R.id.edtH);
+        EditText edtW = (EditText) findViewById(R.id.edtW);
         Button btnCal = (Button) findViewById(R.id.btnCal);
         Button btnClear = (Button) findViewById(R.id.btnClear);
-        btnCal.setOnClickListener(this);
-        btnClear.setOnClickListener(this);
+        btnCal.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                double height = Double.parseDouble(edtH.getText().toString());
+                double weight = Double.parseDouble(edtW.getText().toString());
+                double bmi = weight/ Math.pow(height/100.0, 2); //身高換算公尺再平方
+                if (bmi >=24)
+                    txvShow.setTextColor(Color.RED);
+                else if (bmi<18.5)
+                    txvShow.setTextColor(Color.BLUE);
+                else
+                    txvShow.setTextColor(Color.GREEN);
+
+                txvShow.setText(String.format("%.2f", bmi));
+            }
+        });
+        btnClear.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                edtH.setText("0");
+                edtW.setText("0");
+                txvShow.setText("");
+            }
+        });
     }
+//    @Override
+//    public void onClick(View v) {
+//    EditText edtH = (EditText) findViewById(R.id.edtH);
+//    EditText edtW = (EditText) findViewById(R.id.edtW);
+//
+//    if(v.getId() == R.id.btnCal){
+//        double height = Double.parseDouble(edtH.getText().toString());
+//        double weight = Double.parseDouble(edtW.getText().toString());
+//        double bmi = weight/ Math.pow(height/100.0, 2); //身高換算公尺再平方
+//        if (bmi >=24)
+//            txvShow.setTextColor(Color.RED);
+//        else if (bmi<18.5)
+//            txvShow.setTextColor(Color.BLUE);
+//        else
+//            txvShow.setTextColor(Color.GREEN);
+//
+//        txvShow.setText(String.format("%.2f", bmi));
+//    }else{
+//        edtH.setText("0");
+//        edtW.setText("0");
+//        txvShow.setText("");
+//        }
+//    }
 
-
-
-    @Override
-    public void onClick(View v) {
-    EditText edtH = (EditText) findViewById(R.id.edtH);
-    EditText edtW = (EditText) findViewById(R.id.edtW);
-
-    if(v.getId() == R.id.btnCal){
-        double height = Double.parseDouble(edtH.getText().toString());
-        double weight = Double.parseDouble(edtW.getText().toString());
-        double bmi = weight/ Math.pow(height/100.0, 2); //身高換算公尺再平方
-        if (bmi >=24)
-            txvShow.setTextColor(Color.RED);
-        else if (bmi<18.5)
-            txvShow.setTextColor(Color.BLUE);
-        else
-            txvShow.setTextColor(Color.GREEN);
-
-        txvShow.setText(String.format("%.2f", bmi));
-    }else{
-        edtH.setText("0");
-        edtW.setText("0");
-        txvShow.setText("");
-        }
-    }
 }
