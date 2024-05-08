50122a20722f9081018ad2ed5e23b8de31859462
12DonFun3 <a111221038@mail.shu.edu.tw>
2024-05-08 15:05:22 +0800

    0508

--- 0502/ImageViewDemo/app/src/main/java/com/example/imageviewdemo/MainActivity.java
+++ 0502/ImageViewDemo/app/src/main/java/com/example/imageviewdemo/MainActivity.java
@@ -1,7 +1,7 @@
 package com.example.imageviewdemo;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.util.Log;
 import android.os.Bundle;
 import android.widget.ImageView;
 import android.widget.RadioGroup;
@@ -22,22 +22,28 @@ public class MainActivity extends AppCompatActivity {
         rg.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(RadioGroup group, int checkedId) {
+                Log.d("MainActivity", "RadioButton checked: " + checkedId);
+
                 if (checkedId == R.id.rdbDog) {
+                    Log.d("MainActivity", "Displaying dog image");
                     image1.setVisibility(ImageView.VISIBLE);
                     image2.setVisibility(ImageView.GONE);
                     image3.setVisibility(ImageView.GONE);
                     image4.setVisibility(ImageView.GONE);
                 } else if (checkedId == R.id.rdbElephant) {
+                    Log.d("MainActivity", "Displaying elephant image");
                     image1.setVisibility(ImageView.GONE);
                     image2.setVisibility(ImageView.VISIBLE);
                     image3.setVisibility(ImageView.GONE);
                     image4.setVisibility(ImageView.GONE);
                 } else if (checkedId == R.id.rdbMouse) {
+                    Log.d("MainActivity", "Displaying mouse image");
                     image1.setVisibility(ImageView.GONE);
                     image2.setVisibility(ImageView.GONE);
                     image3.setVisibility(ImageView.VISIBLE);
                     image4.setVisibility(ImageView.GONE);
                 } else if (checkedId == R.id.rdbRabbit) {
+                    Log.d("MainActivity", "Displaying rabbit image");
                     image1.setVisibility(ImageView.GONE);
                     image2.setVisibility(ImageView.GONE);
                     image3.setVisibility(ImageView.GONE);
