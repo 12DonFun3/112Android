e618fd54d0d91b30a1db63ce0eddf58c008d4418
12DonFun3 <a111221038@mail.shu.edu.tw>
2024-05-16 16:51:58 +0800

    3

---
+++ 0516/ListView/app/src/main/java/com/example/listview/MainActivity.java
@@ -0,0 +1,40 @@
+package com.example.listview;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ListView lv;
+    private String[] cities;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        cities = getResources().getStringArray(R.array.cities);
+        lv = (ListView) findViewById(R.id.listview);
+
+        ArrayAdapter<String> adpCities = new ArrayAdapter<>(this,
+                android.R.layout.simple_spinner_item, cities);
+
+        // 将适配器设置到ListView上
+        lv.setAdapter(adpCities);
+
+        lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                // 使用参数parent获取所选项目
+                String selectedCity = parent.getItemAtPosition(position).toString();
+                TextView output = (TextView) findViewById(R.id.lblOutput);
+                output.setText("你是住在" + selectedCity);
+            }
+        });
+    }
+}
